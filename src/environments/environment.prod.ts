44e91826bc0935523b8df404ead3be5b1c5ce208
capem76 <capem76@hotmail.com>
2021-10-30 23:01:04 +0200

    cambios en las rutas de las fotos para se reconozca la ruta desde el servidor de prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBOWn_kEekvw7BF9j0Kr6qtuavdgCWO0PA",
+    authDomain: "portfolio-messages-bf7d8.firebaseapp.com",
+    projectId: "portfolio-messages-bf7d8",
+    storageBucket: "portfolio-messages-bf7d8.appspot.com",
+    messagingSenderId: "726367821695",
+    appId: "1:726367821695:web:efb9ce72cb67015044444a",
+    databaseURL: "https://portfolio-messages-bf7d8-default-rtdb.europe-west1.firebasedatabase.app"
+  }
 };
